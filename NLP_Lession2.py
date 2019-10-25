4d71e918a2e4458fd5d994a8543e1b7805a924d7
xuwenai <etude415@163.com>
2019-10-25 18:46:42 +0800

    Update NLP_Lession2.py
    commit

--- NLP_Lession2.py
+++ NLP_Lession2.py
@@ -14,17 +14,21 @@ response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 
-# json字符串转换为python字典对象
+#json字符串转换为python字典对象
 html_dict = json.loads(html)
 content_json=jsonpath.jsonpath(html_dict,expr='$..st[*]')
-print(content_json)
+#print(content_json)
+str=content_json[0]
+#print(str)
 
-coutent_tmp=['' if x in('[',']') else x for x in content_json]
-print(coutent_tmp)
 
+content_json_new = json.dumps(content_json,ensure_ascii=False) # dict 变 json
+print(content_json_new)
 
 # print(str) #['xiaohei'] 返回的是一个列表
 # print(html)
 # t = jsonpath.jsonpath(dic,'$..s')   #当不存在hehe这个key时，返回false
 # print(t)  #False
 
+str=pd.read_json(content_json_new,orient='records')
+print(str)
